c10d218b95c2a29eee3a15e18e97cfb33d5c13a2
Oleksii Serdiukov <oleksii.serdiukov@apriorit.com>
2024-07-13 15:25:01 +0300

    todo notes ownership

--- app/models/todo.ts
+++ app/models/todo.ts
@@ -1,5 +1,10 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { errors } from '@adonisjs/auth'
+import { BaseModel, column, beforeCreate, afterFind, beforeUpdate, beforeDelete, beforeFetch } from '@adonisjs/lucid/orm'
+import type { ModelQueryBuilderContract } from '@adonisjs/lucid/types/model'
+import { HttpContext } from '@adonisjs/core/http'
+
+const forbiddenError = () => new errors.E_UNAUTHORIZED_ACCESS("Forbidden", { guardDriverName: 'jwt' })
 
 export interface ITodoPayload {
   note: string
@@ -20,9 +25,50 @@ export default class Todo extends BaseModel implements ITodo {
   @column()
   declare note: string
 
+  @column()
+  declare owner: number
+
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   declare updatedAt: DateTime
+
+  @beforeCreate()
+  static onBeforeCreate(todo: Todo) {
+    todo.owner = this.getCurrentUserId();
+  }
+
+  @afterFind()
+  static onAfterFind(todo: Todo) {
+    if (todo.owner !== this.getCurrentUserId()) {
+      throw forbiddenError()
+    }
+  }
+
+  @beforeUpdate()
+  static onBeforeUpdate(todo: Todo) {
+    this.onAfterFind(todo)
+  }
+
+  @beforeDelete()
+  static onBeforeDelete(todo: Todo) {
+    this.onAfterFind(todo)
+  }
+
+  @beforeFetch()
+  static onBeforeFetch(query: ModelQueryBuilderContract<typeof Todo>) {
+    query.where('owner', this.getCurrentUserId())
+  }
+
+  private static getCurrentUserId(): number {
+    const { auth } = HttpContext.getOrFail()
+    const userId = auth?.user?.id
+
+    if (!userId) {
+      throw forbiddenError()
+    }
+
+    return userId
+  }
 }
